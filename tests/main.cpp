9b8cf9048d030906ec8d61afaecc877effd70eda
Batel Oved <100batel100@gmail.com>
2022-01-13 21:40:38 +0200

    add enum usage example in tests/main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,6 +10,7 @@ int main() {
     uf.Union(2,1);
     assert(uf.Find(1)==2);
     assert(uf.Find(2)==2);
+    assert(uf.Find(8)==uf.FAILURE);
     uf.Union(4,3);
     uf.Union(4,2);
     assert(uf.Find(1)==4);
